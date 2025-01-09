969de8b588ca35075380e823a7af2d4192026d57
MEETCHI23 <imric.riva-karaka-abderemane@etu.univ-amu.fr>
2025-01-09 09:51:19 +0100

    Add files via upload

---
+++ page/script.js
@@ -0,0 +1,16 @@
+// script.js
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+});
+
+// Appliquer le thème enregistré
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
